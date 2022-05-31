69d45a4af55fc0046d11539db7079dc6cc5feb69
OJ Kwon <kwon.ohjoong@gmail.com>
2022-05-31 01:54:24 +0000

    feat(visitor): support relative_cwd config

--- spec/swc-reanimated-custom-transform/src/lib.rs
+++ spec/swc-reanimated-custom-transform/src/lib.rs
@@ -91,8 +91,9 @@ pub fn transform_sync(s: String, _is_module: bool, opts: Buffer) -> napi::Result
                     &options,
                     |_program, _comments| {
                         as_folder(create_worklets_visitor(
-                            WorkletsOptions::new(None, filename.clone())
-                            ,c.cm.clone()
+                            WorkletsOptions::new(None, filename.clone()),
+                            c.cm.clone(),
+                            None,
                         ))
                     },
                     |_, _| noop(),
